48bbc2457843393492d27aae3795f5cd869cc9d8
Amarillo2410 <225398294+Amarillo2410@users.noreply.github.com>
2025-09-25 16:19:08 -0500

    feat: :rocket: Archivos try #1

---
+++ js/Archivos.js
@@ -0,0 +1,14 @@
+const lastSafeDownload = 1670000000
+
+const droneLogs = [
+    [8, 1670000700],
+    [42, 1670000500],
+    [13, 1670000100]
+    [8, 1670000001],
+    [1670000000],
+    [99, 1669999999],
+]
+
+function getCompromisedFiles(lastSafeDownload, droneLogs) {
+    
+}
