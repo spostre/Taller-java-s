cb5c9609a11780728eb5ffb3e356445cdc5ead01
Amarillo2410 <225398294+Amarillo2410@users.noreply.github.com>
2025-09-25 15:13:39 -0500

    feat: :tada: Arkanus try #1

---
+++ js/Arkanus.js
@@ -0,0 +1,41 @@
+let moonSymbol = 1
+let moonSymbolReversed = 5
+let femaleSymbol = 10
+let strangeSymbol = 50 
+
+let caldero1 = prompt("Ingrese uno o varios simbolos ♁/☾/☽/⚕/Nan")
+
+let caldero2 = prompt("Ingrese uno o varios simbolos ♁/☾/☽/⚕/Nan")
+
+let caldero3 = prompt("Ingrese uno o varios simbolos ♁/☾/☽/⚕/Nan")
+
+let caldero4 = prompt("Ingrese uno o varios simbolos ♁/☾/☽/⚕/Nan")
+
+let caldero5 = prompt("Ingrese uno o varios simbolos ♁/☾/☽/⚕/Nan")
+
+let result = []
+
+if ("☽" in caldero1, caldero2, caldero3, caldero4, caldero5) {
+    1 += result
+}
+
+if ("☾" in caldero1, caldero2, caldero3, caldero4, caldero5) {
+    5 += result
+}
+
+if ("♁" in caldero1, caldero2, caldero3, caldero4, caldero5) {
+    10 += result
+}
+
+if ("⚕" in caldero1, caldero2, caldero3, caldero4, caldero5) {
+    50 += result
+}
+
+function suma(moonSymbol, moonSymbolReversed, femaleSymbol, strangeSymbol) {
+    return  + b;
+}
+
+
+alert (result)
+console.log (result)
+
